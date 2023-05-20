d62abf133b73f0452c1449d61ddb71d6bc2267c4
Misdar <50019528+misdarmanto@users.noreply.github.com>
2023-05-20 22:48:56 +0700

    Update index.ts

--- app/routers/index.ts
+++ app/routers/index.ts
@@ -2,9 +2,6 @@ import express, { Express, Request, Response } from "express";
 import { index } from "../controllers";
 
 import { middleware } from "../middlewares";
-import { MENU } from "../controllers/menus";
-import { EMPLOYEE } from "../controllers/employee";
-import { FEED_BACK } from "../controllers/feed-back";
 
 export const route = (app: Express) => {
 	app.get("/", middleware.useAuthorization, (req: Request, res: Response) =>
